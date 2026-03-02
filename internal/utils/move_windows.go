749e59871e69b5070e94063360c25ef391829bcc
Maksim Zayakin <rasetsuq@yandex.ru>
2026-03-02 16:59:19 +0500

    refactor: move copyFile to utils package

--- internal/utils/move_windows.go
+++ internal/utils/move_windows.go
@@ -4,9 +4,7 @@ package utils
 
 import (
 	"fmt"
-	"io"
 	"os"
-	"path/filepath"
 	"syscall"
 )
 
@@ -32,52 +30,8 @@ func MoveDir(src, dst string) error {
 	}
 
 	// Fall back to copy; leave originals in tmp.
-	if err := copyDir(src, dst); err != nil {
+	if err := CopyDir(src, dst); err != nil {
 		return fmt.Errorf("failed to copy %s -> %s: %w", src, dst, err)
 	}
 	return nil
-}
-
-func copyDir(src, dst string) error {
-	if err := os.MkdirAll(dst, 0o755); err != nil {
-		return err
-	}
-
-	entries, err := os.ReadDir(src)
-	if err != nil {
-		return err
-	}
-
-	for _, entry := range entries {
-		srcPath := filepath.Join(src, entry.Name())
-		dstPath := filepath.Join(dst, entry.Name())
-
-		if entry.IsDir() {
-			if err := copyDir(srcPath, dstPath); err != nil {
-				return err
-			}
-		} else {
-			if err := copyFile(srcPath, dstPath); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-func copyFile(src, dst string) error {
-	in, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer in.Close()
-
-	out, err := os.Create(dst)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	_, err = io.Copy(out, in)
-	return err
 }
